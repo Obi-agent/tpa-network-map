d2383ecb2c6fcb5a849958876ac0ea2f74d2d142
Obi-agent <agentacc.leo@gmail.com>
2026-04-27 11:39:29 +0100

    Enable Google Sheets provider sync endpoint

--- sheets_config.js
+++ sheets_config.js
@@ -1,4 +1,4 @@
 window.providerSheetsConfig = {
-  enabled: false,
-  appsScriptUrl: '',
+  enabled: true,
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxx9RpSKFORjFLP0m54PtGBdfKTeAFw3J4f3tFE_xJL9eGHWu6d-DcYLJKQDWlls6f7/exec',
 };
