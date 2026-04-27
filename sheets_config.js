3df6c82750af2e1112763cb8d8cedbed18a54fa2
Obi-agent <agentacc.leo@gmail.com>
2026-04-27 16:16:44 +0100

    Update Apps Script endpoint for phone text fix

--- sheets_config.js
+++ sheets_config.js
@@ -1,4 +1,4 @@
 window.providerSheetsConfig = {
   enabled: true,
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw45iNpQyJLHVuQO-LWsvmy2SnTwJWgmOjvVqwcJkOz25zyEe9I7bmsSo0RtE6MDJLm/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw7eiC_PVkhevczu_y-w8KHF7RtFzSKugnqvmmLUbzpihXh_spRsi3cWoWOjCTT3MTp/exec',
 };
