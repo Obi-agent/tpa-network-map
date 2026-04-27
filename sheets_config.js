fbf0f41c97c9e23b6dfdd08e5f8fff6f6130cb57
Obi-agent <agentacc.leo@gmail.com>
2026-04-27 14:47:33 +0100

    Update Google Sheets web app endpoint

--- sheets_config.js
+++ sheets_config.js
@@ -1,4 +1,4 @@
 window.providerSheetsConfig = {
   enabled: true,
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxx9RpSKFORjFLP0m54PtGBdfKTeAFw3J4f3tFE_xJL9eGHWu6d-DcYLJKQDWlls6f7/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxFiesZSO6M_KXf14tbZggE08nH1GoiXkGuzgzQitCZZDtZVOmsFBKiA70evV2bQxES/exec',
 };
